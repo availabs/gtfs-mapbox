20adf2c3d4c02f4bc0d47c3bc28ec1d10c9d08fe
cakesofwrath <dai.alan.dai@gmail.com>
2016-05-25 17:57:29 -0400

    start gtfs integr

--- app/components/GTFSMap/index.js
+++ app/components/GTFSMap/index.js
@@ -1,26 +1,92 @@
 import React, { PropTypes } from "react";
 import MapGL from "react-map-gl";
+import * as request from "superagent";
 const assign = require('object-assign');
+const apiUrl = "http://api.availabs.org/gtfs";
 
 class GTFSMap extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            current_agency: null,
+            loading_agencies: true,
+            agencies: [],
+            routes: [],
+            stops: [],
+            stopNames: [],
+            loaded_agency: {},
+            loaded: false,
+            loading: false,
+            dt: new Date(),
             viewport: {
-                latitude: 37.78,
-                longitude: -122.45,
-                zoom: 11,
+                latitude: 39,
+                longitude: -95,
+                zoom: 5,
                 width: 800,
                 height: 800,
                 startDragLngLat: null,
                 isDragging: null
               }
         }
-        this._onChangeViewport = this._onChangeViewport.bind(this)
+        request.get(apiUrl + "agency", (res) => {
+            let data = JSON.parse(res.text);
+            this.agencies = data;
+            this.loading_agencies = true;
+        })
     }
-    _onChangeViewport(newViewport) {
+
+    _onChangeViewport = (newViewport) => {
         var viewport = assign({}, this.state.viewport, newViewport);
         this.setState({viewport});
+    }
+
+    loadAgency = (agency_id) => {
+        this.current_agency = agency_id;
+        this.loading = true;
+        this.agencies.forEach((agency) => {
+            if(agency.id == agency_id) {
+                this.loaded_agency = agency;
+            }
+        });
+        loadRoutes(agency_id, () => {
+            loadStops(agency_id, () => {
+                this.loading = false;
+                this.loaded = true;
+            });
+        });
+    }
+
+    loadRouteInfo = (route_id) => {
+        this.route_info = route_id;
+        request.post(apiUrl + "agency/" + this.current_agency + "/routes/" + this.route_id + "/schedule")
+            .send({
+                "day": days[this.dt.getDay()]
+            })
+            .end((res) => {
+                let data = JSON.parse(res.text);
+                let direction = {},
+                    stop = [];
+                data.forEach((point) => {
+                    if(point.direction_id == null) {
+                        point.direction_id = 0;
+                    }
+
+                    if(typeof direction[point.direction_id] == "undefined") {
+                        direction[point.direction_id] = { trips: {} };
+                    }
+
+                    if(typeof direction[point.direction_id].trips[point.trip_id] == "undefined") {
+                        direction[point.direction_id].trips[point.trip_id] = {};
+                    }
+
+                    let stop = point.stop_id,
+                        time = point.arrival_time;
+
+                    direction[point.direction_id].trips[point.trip_id][stop] = time;
+                });
+                this.direction = direction;
+                let zeroStops = {};
+            })
     }
 
     render() {
